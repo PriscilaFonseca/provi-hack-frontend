a281c3c69ca831c5989ed1def69fde95f09116d5
Larissa Diniz <larissa.diniz_23@hotmail.com>
2021-02-07 13:31:34 -0300

    cores por stack

--- scrypts/desafios.js
+++ scrypts/desafios.js
@@ -1,14 +1,36 @@
 const divCards = document.getElementById('cards')
 var cardAtual;
 var novoCard;
+var corStack;
 
 window.addEventListener('load', function desafio(){
 
-    fetch('https://fakestoreapi.com/products?limit=5')
+    fetch('https://fakestoreapi.com/products?limit=6')
             .then(res => res.json())
             .then(json => {
                 json.forEach(desafio => {
                     console.log(desafio)
+
+                    /* cor por tipo de stack */
+                    switch(desafio.category) {
+                        case "frontend":
+                            corStack = "front"
+                            break;
+                        case "backend":
+                            corStack = "back"
+                            break;
+                        case "mobile":
+                            corStack = "mob"
+                            break;
+                        case "data":
+                            corStack = "data"
+                            break;
+                        case "ui/ux":
+                            corStack = "ui"
+                            break;  
+                        default:
+                            corStack = "default"
+                    }
 
                     cardAtual = divCards.innerHTML;
 
@@ -16,7 +38,7 @@ window.addEventListener('load', function desafio(){
                     <div class="desafio-card">
                         <a class="d-flex justify-content-between align-items-center link-desafio" href="./pagina-desafio.html?id=${desafio.id}">
                             <p class="font-3">${desafio.title}</p>
-                            <div class="stack font-4 d-flex justify-content-center align-items-center"> ${desafio.category.toUpperCase()} </div>
+                            <div class="stack ${corStack} font-4 d-flex justify-content-center align-items-center"> ${desafio.category.toUpperCase()} </div>
                         </a>
                     </div>
                     `
